9bc481f6b9e6985cb4fa8c4ae76192640878e43e
hellacopters <salpesce@gmail.com>
2023-11-28 06:40:41 -0500

    updated exports

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -7,7 +7,7 @@ export { decodeBC6, decodeBC6H, decodeBC6S } from './bc6';
 export { decodeBC7 } from './bc7';
 export { getCRNMeta, decodeCRN } from './crn2';
 export { makeTGA, TGA_PROFILE } from './tga_maker';
-export { readPNG, makePNG, makePNG2 } from './png_maker';
+export { readPNG, makePNG, } from './png_maker';
 export { COLOR_PROFILE, BYTE_VALUE, convertProfile, } from './profiler';
 export { flipImage, cropImage } from './flipper';
 export { inflate, Inflate, deflate, Deflate, deflateRaw, inflateRaw, gzip, ungzip } from './deflate/index';
